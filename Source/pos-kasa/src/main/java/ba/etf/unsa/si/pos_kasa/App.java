37fd1342e494db43daa80a74ea4761392c4835e7
Vejsil Hrustic <vhrustic1@etf.unsa.ba>
2016-04-24 15:44:16 +0200

    Inicijalni Maven-Java projekat

---
+++ Source/pos-kasa/src/main/java/ba/etf/unsa/si/pos_kasa/App.java
@@ -0,0 +1,13 @@
+package ba.etf.unsa.si.pos_kasa;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
