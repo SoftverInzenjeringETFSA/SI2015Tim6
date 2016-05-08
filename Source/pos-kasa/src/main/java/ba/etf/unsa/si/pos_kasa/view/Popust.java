d014ece09bfadcaa9fb7fb560c457163c6570d2f
toshiba <toshiba@toshiba-PC>
2016-05-08 16:09:30 +0200

    prepravka forme za definisanje popusta

--- Source/pos-kasa/src/main/java/ba/etf/unsa/si/pos_kasa/view/Popust.java
+++ Source/pos-kasa/src/main/java/ba/etf/unsa/si/pos_kasa/view/Popust.java
@@ -15,13 +15,10 @@ import com.toedter.calendar.JDateChooser;
 public class Popust {
 
 	private JFrame frmPopust;
-	private JTextField txtIznosRacuna;
-	private JTextField txtKasir;
-	private JLabel lblPopust;
 	private JLabel lblIznosRaunaS;
-	private JTextField txtIznosRacunaSPopustom;
 	private JTextField txtIznosPopusta;
 	private JButton btnKreirajPopust;
+	private JLabel label;
 
 	/**
 	 * Launch the application.
@@ -42,6 +39,7 @@ public class Popust {
 	/**
 	 * Create the application.
 	 */
+	
 	public Popust() {
 		initialize();
 	}
@@ -53,29 +51,10 @@ public class Popust {
 	private void initialize() {
 		frmPopust = new JFrame();
 		frmPopust.setTitle("Popust");
-		frmPopust.setBounds(100, 100, 652, 234);
+		frmPopust.setBounds(100, 100, 310, 234);
 		frmPopust.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
-		txtIznosRacuna = new JTextField();
-		txtIznosRacuna.setColumns(10);
-		
-		txtKasir = new JTextField();
-		txtKasir.setColumns(10);
-		
-		JLabel lblNewLabel = new JLabel("Iznos računa:");
-		
-		JLabel lblNazivNekogArtikla = new JLabel("Naziv nekog artikla");
-		
-		JLabel lblBarkodArtikla = new JLabel("Barkod artikla");
-		
-		JLabel lblKasir = new JLabel("Kasir:");
-		
-		lblPopust = new JLabel("Popust:");
-		
-		lblIznosRaunaS = new JLabel("Iznos računa s popustom:");
-		
-		txtIznosRacunaSPopustom = new JTextField();
-		txtIznosRacunaSPopustom.setColumns(10);
+		lblIznosRaunaS = new JLabel("Iznos popusta:");
 		
 		txtIznosPopusta = new JTextField();
 		txtIznosPopusta.setColumns(10);
@@ -83,68 +62,47 @@ public class Popust {
 		btnKreirajPopust = new JButton("Kreiraj Popust");
 		
 		JDateChooser dateChooser = new JDateChooser();
+		
+		JLabel lblOdaberiteDatum = new JLabel("Odaberite Datum:");
+		
+		label = new JLabel("%");
 		GroupLayout groupLayout = new GroupLayout(frmPopust.getContentPane());
 		groupLayout.setHorizontalGroup(
-			groupLayout.createParallelGroup(Alignment.TRAILING)
-				.addGroup(Alignment.LEADING, groupLayout.createSequentialGroup()
-					.addGap(37)
+			groupLayout.createParallelGroup(Alignment.LEADING)
+				.addGroup(groupLayout.createSequentialGroup()
 					.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
-						.addComponent(lblBarkodArtikla)
 						.addGroup(groupLayout.createSequentialGroup()
+							.addGap(37)
 							.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
-								.addComponent(lblNazivNekogArtikla)
-								.addComponent(dateChooser, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
-							.addGap(83)
+								.addComponent(lblOdaberiteDatum)
+								.addComponent(lblIznosRaunaS))
+							.addPreferredGap(ComponentPlacement.UNRELATED)
 							.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
-								.addComponent(btnKreirajPopust)
 								.addGroup(groupLayout.createSequentialGroup()
-									.addGroup(groupLayout.createParallelGroup(Alignment.LEADING, false)
-										.addGroup(groupLayout.createSequentialGroup()
-											.addComponent(lblKasir)
-											.addPreferredGap(ComponentPlacement.RELATED, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-											.addComponent(txtKasir, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
-										.addGroup(groupLayout.createSequentialGroup()
-											.addGap(1)
-											.addComponent(lblNewLabel)
-											.addGap(18)
-											.addComponent(txtIznosRacuna, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)))
-									.addGap(18)
-									.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
-										.addComponent(lblIznosRaunaS)
-										.addComponent(lblPopust))
+									.addComponent(txtIznosPopusta, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
 									.addPreferredGap(ComponentPlacement.RELATED)
-									.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
-										.addComponent(txtIznosPopusta, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
-										.addComponent(txtIznosRacunaSPopustom, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))))))
-					.addContainerGap(37, Short.MAX_VALUE))
+									.addComponent(label))
+								.addComponent(dateChooser, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)))
+						.addGroup(groupLayout.createSequentialGroup()
+							.addGap(65)
+							.addComponent(btnKreirajPopust, GroupLayout.PREFERRED_SIZE, 123, GroupLayout.PREFERRED_SIZE)))
+					.addContainerGap(25, Short.MAX_VALUE))
 		);
 		groupLayout.setVerticalGroup(
 			groupLayout.createParallelGroup(Alignment.LEADING)
 				.addGroup(groupLayout.createSequentialGroup()
-					.addGap(33)
+					.addGap(27)
 					.addGroup(groupLayout.createParallelGroup(Alignment.TRAILING)
-						.addComponent(lblKasir)
-						.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
-							.addGroup(groupLayout.createSequentialGroup()
-								.addComponent(lblNazivNekogArtikla)
-								.addGap(18)
-								.addComponent(lblBarkodArtikla))
-							.addGroup(groupLayout.createSequentialGroup()
-								.addGroup(groupLayout.createParallelGroup(Alignment.BASELINE)
-									.addComponent(txtIznosRacuna, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
-									.addComponent(lblNewLabel)
-									.addComponent(lblPopust)
-									.addComponent(txtIznosPopusta, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
-								.addGap(18)
-								.addGroup(groupLayout.createParallelGroup(Alignment.BASELINE)
-									.addComponent(txtKasir, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
-									.addComponent(lblIznosRaunaS)
-									.addComponent(txtIznosRacunaSPopustom, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)))))
-					.addGap(41)
-					.addGroup(groupLayout.createParallelGroup(Alignment.TRAILING)
-						.addComponent(dateChooser, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
-						.addComponent(btnKreirajPopust))
-					.addContainerGap(169, Short.MAX_VALUE))
+						.addComponent(lblOdaberiteDatum)
+						.addComponent(dateChooser, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+					.addGap(18)
+					.addGroup(groupLayout.createParallelGroup(Alignment.BASELINE)
+						.addComponent(lblIznosRaunaS)
+						.addComponent(txtIznosPopusta, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+						.addComponent(label))
+					.addGap(18)
+					.addComponent(btnKreirajPopust)
+					.addContainerGap(70, Short.MAX_VALUE))
 		);
 		frmPopust.getContentPane().setLayout(groupLayout);
 	}
