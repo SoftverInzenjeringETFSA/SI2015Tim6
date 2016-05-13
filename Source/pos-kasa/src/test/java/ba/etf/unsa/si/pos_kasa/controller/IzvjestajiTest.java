745049266e783f6909615e53623b14d9ad402338
Ramiza Hoso <rhoso1@etf.unsa.ba>
2016-05-13 23:59:42 +0200

    popravka

--- Source/pos-kasa/src/test/java/ba/etf/unsa/si/pos_kasa/controller/IzvjestajiTest.java
+++ Source/pos-kasa/src/test/java/ba/etf/unsa/si/pos_kasa/controller/IzvjestajiTest.java
@@ -22,6 +22,6 @@ public class IzvjestajiTest extends TestCase {
 	     //
 	     double [] _rezultat = i.dajUkupanPrometIprodateArtikleOdDo(datum1, datum2);
 	 
-	   assertEquals(0,_rezultat[0]);  
+	   assertEquals(0.0,_rezultat[0]);  
 	}
 }
